c45c83f7c78b5698fc36d0c901c04b4e26b82732
Wayne Miller <wayne.c.miller@comcast.net>
2023-12-18 04:20:07 -0600

    Fixed weapon ranges.

--- Data/Scripts/CoreParts/Missiles_Rocket_Weapons.cs
+++ Data/Scripts/CoreParts/Missiles_Rocket_Weapons.cs
@@ -24,7 +24,7 @@ namespace Scripts
             ClosestFirst = false, // tries to pick closest targets first (blocks on grids, projectiles, etc...).
             IgnoreDumbProjectiles = true, // Don't fire at non-smart projectiles.
             LockedSmartOnly = false, // Only fire at smart projectiles that are locked on to parent grid.
-            MaxTargetDistance = 3000, // 0 = unlimited, Maximum target distance that targets will be automatically shot at.
+            MaxTargetDistance = 4000, // 0 = unlimited, Maximum target distance that targets will be automatically shot at.
             TopTargets = 1, // 0 = unlimited, max number of top targets to randomize between.
             TopBlocks = 4, // 0 = unlimited, max number of blocks to randomize between
             StopTrackingSpeed = 1000, // do not track target threats traveling faster than this speed
